9890ab470e780eeef28078332d25502e4291e92a
tcobb19 <tylercobb2003@yahoo.com>
2019-07-07 21:56:40 -0500

    Upload plots & code

---
+++ usaby_regions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+Midwest  = ['Illinois', 'Indiana', 'Iowa', 'Kentucky', 'Michigan', 'Minnesota', 'Missouri', 'Ohio', 'Wisconsin']
+Northeast = ['Connecticut', 'Delaware', 'District of Columbia', 'Maine', 'Maryland', 'Massachusetts' ,'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania','Rhode Island', 'Vermont', 'Virginia', 'West Virginia']
+Pacific_west = ['Alaska', 'Arizona', 'California', 'Hawaii', 'Idaho', 'Nevada', 'Oregon', 'Utah', 'Washington']
+Plains_regions = ['Colorado', 'Kansas', 'Montana', 'Nebraska', 'New Mexico', 'North Dakota', 'Oklahoma', 'South Dakota', 'Texas', 'Wyoming']
+Southeast = ['Alabama', 'Arkansas', 'Florida', 'Georgia', 'Louisiana', 'Mississippi', 'North Carolina', 'South Carolina', 'Tennessee']
+
+USARegions = {'Midwest': Midwest, 'Northeast': Northeast, 'Pacific_west': Pacific_west, 'Plains_regions': Plains_regions, 'Southeast': Southeast}
+
